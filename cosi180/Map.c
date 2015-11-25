b7195d91a3cef232696f0c45f281915e6f460783
Josh Silverman <joshs.silverman@gmail.com>
2015-11-25 17:55:19 -0500

    working heap insert

--- cosi180/Map.c
+++ cosi180/Map.c
@@ -39,31 +39,54 @@ void PrintLeg(int edge);
 typedef struct {
     unsigned int size;
     unsigned int count;
-    int A[10];
+    unsigned int A[10];
 } Heap;
 
-Heap heap_init(int max_size);
-void heap_insert(int d, Heap H);
+Heap* heap_init(int max_size);
+void heap_insert(int d, Heap *H);
+void heap_percup(unsigned int i, Heap *H);
+unsigned int heap_parent(int i, Heap *H);
+void heap_swap(int i, int j, Heap *H);
 
-Heap heap_init(int max_size) {
-    Heap H;
-    H.size = max_size;
-    H.count = 0;
+Heap* heap_init(int max_size) {
+    Heap *H = malloc(sizeof(Heap));
+    (*H).size = max_size;
+    (*H).count = 0;
     
     return H;
 };
 
-void heap_insert(int d, Heap H){
-    printf("insert: %i\n", d);
-    H.A[H.count] = d;
-    H.count++;
+void heap_insert(int d, Heap *H){
+    (*H).A[(*H).count] = d;
+    printf("%2d\n", (*H).A[(*H).count]);
+    heap_percup((*H).count, H);
+    (*H).count = (*H).count + 1;
     
-    printf("value at location %i is %i\n", H.count-1, d);
+    printf("value at location %i is %i\n", (*H).count-1, d);
 };
 
-void heap_percup(int i, Heap H) {
-    
+void heap_percup(unsigned int i, Heap *H) {
+    unsigned int parent_i = heap_parent(i, H);
+    printf("parent 0: %2d\n", parent_i);
+    while ((*H).A[parent_i] > (*H).A[i]) {
+        heap_swap(i, parent_i, H);
+    }
 }
+
+unsigned int heap_parent(int i, Heap *H){
+    if (i == 0) {
+        return i;
+    } else {
+        return (unsigned int)(round((float)i/2.0f) - 1);
+    };
+};
+
+void heap_swap(int i, int j, Heap *H){
+    printf("swap: %2d and %2d\n", (*H).A[i], (*H).A[j]);
+    int tmp = (*H).A[i];
+    (*H).A[i] = (*H).A[j];
+    (*H).A[j] = tmp;
+};
 
 /***************************************************************************************/
 /*Dijkstra Algorithm                                                                   */
@@ -85,8 +108,21 @@ void Dijkstra(int DijkstraFlag) {
 /***************************************************************************************/
 int main() {
     
-    Heap H = heap_init(1);
+    Heap *H = heap_init(1);
+    
     heap_insert(10, H);
+    printf("at 0: %2d\n", (*H).A[0]);
+    
+    heap_insert(8, H);
+    heap_insert(11, H);
+    heap_insert(24, H);
+
+    printf("count: %2d\n", (*H).count);
+    
+    printf("at 0: %2d\n", (*H).A[0]);
+    printf("at 1: %2d\n", (*H).A[1]);
+    printf("at 1: %2d\n", (*H).A[3]);
+    printf("at 1: %2d\n", (*H).A[4]);
     
 /* GetVertices();*/
 /* GetEdges();*/
